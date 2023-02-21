809d852a1283abbb64e25c6a5bec49b88609d234
M4TThts123 <70703948+M4TThys123@users.noreply.github.com>
2023-02-21 18:22:40 +0100

    Made a nav and dynamic data

--- main.js
+++ main.js
@@ -1,7 +1,55 @@
-var app = new Vue({
+const app = new Vue({
     el: '#app',
     data: {
+        // Infomratie
         product: 'Socks',
-        description: 'A pair of warm fuzzy socks'
+        description: 'A pair of warm fuzzy socks',
+        inventory: 11,
+
+        // Foto
+        image: './assets/sock.jpeg',
+
+        // Eigenschappen
+        details: ['80% cutton', '20 poleyester', 'Top-G'],
+        variants: [
+            {
+                Id: 0,
+                Color: 'Green'
+            },
+            {
+                Id: 0,
+                Color: 'Blue'
+            }
+        ]
+
     }
 })
+
+const nav = new Vue({
+    el: '#nav',
+    data: {
+        // Header image
+        logo: './assets/sock.jpeg',
+
+        navigaion: [
+            {
+                url: '/',
+                name: 'Home'
+            },
+            {
+                url: '/about',
+                name: 'Diensten'
+            },
+            {
+                url: '/about',
+                name: 'Partners'
+            },
+            {
+                url: '/about',
+                name: 'About'
+            },
+        ]
+    }
+})
+
+// Nav
